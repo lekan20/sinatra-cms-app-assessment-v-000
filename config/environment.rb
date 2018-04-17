062bbf02666d845ed8eed65df15e63d31d74790b
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-04-17 23:51:03 +0000

    Trying to fix config.ru bug

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,22 @@
-require 'bundler'
+# require 'bundler'
+#
+# Bundler.require
+#
+#
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/development.sqlite"
+# )
 
-Bundler.require
 
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/development.sqlite"
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
 require_all 'app'
