9cbbe8b26ab44e94effc51dc5d1774126e649f49
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-04-17 23:32:41 +0000

    Messing aroudn with sessions in application controller

--- app/controller/application_controller.rb
+++ app/controller/application_controller.rb
@@ -3,12 +3,17 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    # enable :sessions
-    # set :session_secret "collection_security"
+    enable :sessions
+    set :session_secret, "collection_security"
   end
 
   get '/' do
+    session[:greeting] = "Hello World"
     "Hello World"
+  end
+
+  get '/remember' do
+    session[:greeting]
   end
 
 end
