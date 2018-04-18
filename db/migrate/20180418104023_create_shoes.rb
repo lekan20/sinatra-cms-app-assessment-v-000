53613647ac3a758e0d7ddba24ddccd701ca214d5
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-04-18 10:44:38 +0000

    Set up create shoes migration

--- db/migrate/20180418104023_create_shoes.rb
+++ db/migrate/20180418104023_create_shoes.rb
@@ -1,4 +1,9 @@
 class CreateShoes < ActiveRecord::Migration[5.2]
   def change
+    create_table :shoes do |t|
+      t.string :name
+      t.string :brand
+      t.integer :user_id
+    end
   end
 end
